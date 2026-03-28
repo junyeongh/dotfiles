f55c5c55730f07405acd4925f7c551740e20f6f0
Junyeong Heo <hhjjyy1122@gmail.com>
2026-03-28 11:48:38 +0900

    2026-03-28

--- home/packages.nix
+++ home/packages.nix
@@ -3,33 +3,15 @@
 # https://search.nixos.org/packages
 with pkgs;
 [
-  bat
   btop
-  ctop
-  delta
-  difftastic
-  direnv
   dotter
-  eza
-  fastfetch
   fnm
-  fzf
   gh
   git
   git-lfs
-  helix
-  hyperfine
-  just
-  lazydocker
-  lazygit
   mise
-  neovim
   nil
   nixfmt
-  oh-my-posh
-  ripgrep
   tmux
-  uv
   zellij
-  zoxide
 ]
