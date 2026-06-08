885bef54cabfcfa28cb319b13bd08977fa3195be
Junyeong Heo <hhjjyy1122@gmail.com>
2026-06-08 17:48:27 +0900

    misc

--- hosts/probook/configuration.nix
+++ hosts/probook/configuration.nix
@@ -128,17 +128,18 @@
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
 
-  programs._1password.enable = true;
-  programs._1password-gui.enable = true;
-  programs._1password-gui.polkitPolicyOwners = [ "yeong" ];
-
+  programs.nix-ld.enable = true;
   programs.git.enable = true;
   programs.git.config = {
     "gpg \"ssh\"" = {
       program = lib.getExe' pkgs._1password-gui "op-ssh-sign";
     };
   };
-  programs.nix-ld.enable = true;
+
+  programs._1password.enable = true;
+  programs._1password-gui.enable = true;
+  programs._1password-gui.polkitPolicyOwners = [ "yeong" ];
+  programs.steam.enable = true;
 
   virtualisation = {
     containers.enable = true;
